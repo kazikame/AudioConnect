88ca4ad66c518296bd1a32c73b2dce4715c43359
Saksham Goel <38839591+kazikame@users.noreply.github.com>
2019-02-08 12:14:11 +0530

    new faster implementation

--- app/src/main/java/com/example/sonogram/MainActivity.java
+++ app/src/main/java/com/example/sonogram/MainActivity.java
@@ -26,6 +26,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.Arrays;
 
 import io.chirp.connect.ChirpConnect;
 import io.chirp.connect.interfaces.ConnectEventListener;
@@ -40,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     String KEY = "A28cBeC94C2f7e8D2eFEc6244";
     String SECRET = "f13f77B53100a95FCA3f61Aa13Cd6ACdfCC8DDccB025659502";
     String CONFIG = "dDp/pK127YGHxWaOydb07S22h2s0Pg7gbqlWWztib/1xM0zX4FOkaHpdzHF+u89XrQ52HecEyXdoj8w96gbJNCTSX0Qyg0LHoM/s0SuLQmr+TF4YXueih3YSV0gZgblP/aQCbtb1vMSkm1ljvU6issYfwrmm8JAg5/W6ZM6wbqdvYMzaRykpEKeoP5c0PEuAhRC0Zd8R0f3TEFIOj6qkF2HCHx9fVSNBrfP+ws/pvDAOIhdr9LK6NS5nv3iwsP/V5csAdIATY5Hx19uJjdmjx/jYGeThv4/+l37kJnl4MJbg9nibBsGeOE4C3mba9L0ytE/jjg2vxy8Hy4KYeUB+6HcbFpZn1tKWyiX+8r3xJuRV0KYP4eZXZvTZDu+oXrGrOaUnMsuxdo1DN1qwtge+SlO/BeDOLo2Nc5mCecDYCt4KYR528c+7fcUQGMDCKZseWqYN9xrzRvAr+N2QLku/Tc8wiBRdcDfZYbLW1fpwvdKeQ4Haac+BiU8HmYRkaOSwi+uE1gtrf3r3AjSprkQ6XEGubbh4Bn8uxRg1bKSKmIC1KgnbNqH+10KlQ/iuR2twzpTLHS/oO6fnQsNQukg9V2lKy/mXzMFtdhU+VQq/Fl3EevebIXxOy9ZO6ln/y+zb7HKZgwvFdyxcj17VDqdmkCTKgQNv+gwfO7jPh/Bi8choPrKdPuQdxrsttR+482OqjeL3UWcWccgUv++me2ZmV7pX6eb+X1aSFoJxBuQdlZoO0J4e0zeEStggUfA5Vq6ru4Vng+LABsp61sNEaprcmuU/vWpNWpSHq+P1qcnducvZPFfmvXyrT2hInbo+hqTOZvlfVFhRXOxm7dm8UyCKKvchuuzUEGQPodbyydZa5+UCR+XSe2OslvcPALxKwNJMorgcwnvL+/DmrYbhNSsc0M6G9QhIloUAtvkHq6pbddv61UEMEtRP+FemKsqATQvrZy6f+wVGG4sfB426j5wTB2R4/m0UGxNkC3UjwOVNMClhUl1UeOpr9d8EV41PmgX7XdFyf0DqpktBK0AZeNttChJEkQdtu/Tik/Ocu92PKgMOYeJBRfhswlkbahx6uKWp8WPJgaQavyA/3rrI48A4yhfvCJHMe8lUWbh3i4NcoSsCPKDNaU718heOYr08OSP/LDUC3KcdIknqGSV0OTPuFML/5xZVfHus3wAktFWZdKUzrnpqUyMl0bbav9fpXxJZemfRSTr0G9pEu/ofmIWwaJPfR09QyK+ZBFo/ffaApGbCbbNgQ4pjOf2NrglAJbqGpGSz5f3ACsn7vKXYvYAOM+1eodJb2mY2a+tk7qDjTN/gDAGgZ4O9oXE3o4ELf5qlqbos56WtU1xsJEzWkyiX8FYdBkvWX/00JL0aY+X89jTkDKP/IhGw+5BobkIO2VshXi9md3MUC4Hym3SB4TWSNAbqBgMY7tMBOfKEaVZTBf3gIrYP4dzOFFW5yXQ0q+ENCeiLn9mloCLOp+xfIpqZbe4vqGdZ3nmSO6jvQ/Iv8o7d5F1YGPNURcI2XPhouHa/32qr9mKpqLSnZk89Xq5HKmpzLDDR5X9YkSe1hesN3CtOochPEzZuEfW2wDP4ujcpm4u0eW3UFkOBPpekKZowJSjXgvw+qG9KvPe/K8iMcrwHNXPKB5SvcAyx8iIkOLw4aDhEvCcBf3ekjdLvefEa7XuvGCfWRiSZEXQZuR7HRfEsffjqInrfn5Yi9DpLbzsN6IyPV1Kj1qu537bK44+VoC56cgDGgIMf8ysL5/gOeM2pqWQ3uQ6iWjUMH4S659BCa9cBmryWhep8gtmUn6/B9wIWiqByPVV4jKPpzwSqAbY0NYYJ/AbtDlEWtlsSOeCZgoRO8kM3eNfW4u83rz+BKqY76cJ9SBfV6oc0x15vyd/6Pc1/pU0tK0z4KGpv8cqnF4mr6E8jLXkLDFakex5yGAJlJnihDR9r02Ygl8vAJN8hJjZwtTk/a+Q/Zeu4KW/ZXhlHb4DVDNww0WFdvtFmHdQQZ+ziTen7l66HckK+uFx2cx6dqilwkIKGeS0cvvCeRryHCOoz2A4qEd3H2j61oOHdb+iEtzxVVZ3F0IWG+3aHCdBPvvOhRwd9WqUK7VxxK0AVRxVUylHt4wyLElnmuB6k1h0NByCzI1n6funSXXLwqJcKvl/2bINOFWA/WoNPOuB+aKEK/1tMfgEpgAIfhPCf9DavNmf6CIT2YAI8futqWM/AXdE7wbAnhEqeyflfuQoxxsgWMTw1e06Y31Q/5ozVx8dTWFlG/eeb0/A+73mPJu97VLO6diexlqMgKZIQ/0YARRccVvHq5r1BhGeM5++aUhGjUdRqHvPXxl1lgeQ0kqTFbxBLRU6O6hPTPR9rNFj4BQmTJG3t4qkO7ARWRXXzqVAQFu6L6DI2hkdqC63vOi6XMYUXpGqp7y0TKE9z6HpFQchhYVUe36hEAiwr+umsN63TJyThqcR+elbFSJgry+MTl0K2NErYADjFdyJUMobMQkqbDLYb+vOyxOY4U/wqELCwRr833WmqedB6OEYMIcP/9hfj0HBPSjMNzJ8DpQzfV34nQjekbzwx87ugEaX/826qHAxkYFhhIXSPQKo=";
-    private static String ACK = "1";
+    private static String ACK = "00";
     private static String NACK = "0";
     private static final int RESULT_REQUEST_RECORD_AUDIO = 0;
 
@@ -134,10 +135,9 @@ public class MainActivity extends AppCompatActivity {
 
                 else if (errorStringLength == 0) {
                     currSequenceNo++;
-                    if (currSequenceNo == 2)
-                        currSequenceNo = -1;
+                    currSequenceNo %= 2;
                     correctText = bitString.getText().toString();
-                    chirpConnect.send((correctText + crc_g(bitStringLength, correctText) + currSequenceNo).getBytes());
+                    chirpConnect.send(padMessage(correctText + crc_g(bitStringLength, correctText) + currSequenceNo));
                 }
 
                 else if (bitStringLength != errorStringLength) {
@@ -157,10 +157,9 @@ public class MainActivity extends AppCompatActivity {
                     correctText = bitString.getText().toString();
 
                     currSequenceNo++;
-                    if (currSequenceNo == 2)
-                        currSequenceNo = -1;
+                    currSequenceNo %= 2;
 
-                    chirpConnect.send((toBeSent + crc_g(bitStringLength, correctText) + currSequenceNo).getBytes());
+                    chirpConnect.send(padMessage(toBeSent + crc_g(bitStringLength, correctText) + currSequenceNo));
                     Toast.makeText(getApplicationContext(), "Sending...", Toast.LENGTH_SHORT).show();
 
                 }
@@ -239,11 +238,12 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         recdBitString = new String(payload);
                     } catch (Exception e) {
+                        final String error = e.getMessage();
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
                                 Toast.makeText(getApplicationContext(),
-                                        "Received NULL :(" ,
+                                        "Received error message: " + error ,
                                         Toast.LENGTH_SHORT).show();
                             }
                         });
@@ -272,13 +272,13 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
                                 Toast.makeText(getApplicationContext(),
-                                        "Received NACK, Retrying!" + (new String(payload)) ,
+                                        "Received NACK, Retrying!" ,
                                         Toast.LENGTH_SHORT).show();
                             }
                         });
                         isTimeoutRunning = false;
                         timeoutThread.quit();
-                        chirpConnect.send((correctText + crc_g(correctText.length(), correctText) + currSequenceNo).getBytes());
+                        chirpConnect.send(padMessage(correctText + crc_g(correctText.length(), correctText) + currSequenceNo));
                     }
 
                     else
@@ -426,5 +426,40 @@ public class MainActivity extends AppCompatActivity {
                 isTimeoutRunning = false;
             }
         });
+    }
+
+    private byte[] padMessage(String message) {
+        int padLength = 8 - (message.length() % 8);
+
+        char[] chars = new char[padLength - 1];
+        Arrays.fill(chars, '0');
+        return getByteByString((new String(chars)) + "1" + message);
+
+    }
+
+    public static byte[] getByteByString(String binaryString) {
+        int splitSize = 8;
+
+        if(binaryString.length() % splitSize == 0){
+            int index = 0;
+            int position = 0;
+
+            byte[] resultByteArray = new byte[binaryString.length()/splitSize];
+            StringBuilder text = new StringBuilder(binaryString);
+
+            while (index < text.length()) {
+                String binaryStringChunk = text.substring(index, Math.min(index + splitSize, text.length()));
+                Integer byteAsInt = Integer.parseInt(binaryStringChunk, 2);
+                resultByteArray[position] = byteAsInt.byteValue();
+                index += splitSize;
+                position ++;
+            }
+            return resultByteArray;
+        }
+        else{
+           Log.v("Error","Cannot convert binary string to byte[], because of the input length. '"
+                   +binaryString+"' % 8 != 0");
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/sonogram/ReceivingActivity.java
+++ app/src/main/java/com/example/sonogram/ReceivingActivity.java
@@ -29,6 +29,8 @@ public class ReceivingActivity extends AppCompatActivity {
     String CONFIG = "dDp/pK127YGHxWaOydb07S22h2s0Pg7gbqlWWztib/1xM0zX4FOkaHpdzHF+u89XrQ52HecEyXdoj8w96gbJNCTSX0Qyg0LHoM/s0SuLQmr+TF4YXueih3YSV0gZgblP/aQCbtb1vMSkm1ljvU6issYfwrmm8JAg5/W6ZM6wbqdvYMzaRykpEKeoP5c0PEuAhRC0Zd8R0f3TEFIOj6qkF2HCHx9fVSNBrfP+ws/pvDAOIhdr9LK6NS5nv3iwsP/V5csAdIATY5Hx19uJjdmjx/jYGeThv4/+l37kJnl4MJbg9nibBsGeOE4C3mba9L0ytE/jjg2vxy8Hy4KYeUB+6HcbFpZn1tKWyiX+8r3xJuRV0KYP4eZXZvTZDu+oXrGrOaUnMsuxdo1DN1qwtge+SlO/BeDOLo2Nc5mCecDYCt4KYR528c+7fcUQGMDCKZseWqYN9xrzRvAr+N2QLku/Tc8wiBRdcDfZYbLW1fpwvdKeQ4Haac+BiU8HmYRkaOSwi+uE1gtrf3r3AjSprkQ6XEGubbh4Bn8uxRg1bKSKmIC1KgnbNqH+10KlQ/iuR2twzpTLHS/oO6fnQsNQukg9V2lKy/mXzMFtdhU+VQq/Fl3EevebIXxOy9ZO6ln/y+zb7HKZgwvFdyxcj17VDqdmkCTKgQNv+gwfO7jPh/Bi8choPrKdPuQdxrsttR+482OqjeL3UWcWccgUv++me2ZmV7pX6eb+X1aSFoJxBuQdlZoO0J4e0zeEStggUfA5Vq6ru4Vng+LABsp61sNEaprcmuU/vWpNWpSHq+P1qcnducvZPFfmvXyrT2hInbo+hqTOZvlfVFhRXOxm7dm8UyCKKvchuuzUEGQPodbyydZa5+UCR+XSe2OslvcPALxKwNJMorgcwnvL+/DmrYbhNSsc0M6G9QhIloUAtvkHq6pbddv61UEMEtRP+FemKsqATQvrZy6f+wVGG4sfB426j5wTB2R4/m0UGxNkC3UjwOVNMClhUl1UeOpr9d8EV41PmgX7XdFyf0DqpktBK0AZeNttChJEkQdtu/Tik/Ocu92PKgMOYeJBRfhswlkbahx6uKWp8WPJgaQavyA/3rrI48A4yhfvCJHMe8lUWbh3i4NcoSsCPKDNaU718heOYr08OSP/LDUC3KcdIknqGSV0OTPuFML/5xZVfHus3wAktFWZdKUzrnpqUyMl0bbav9fpXxJZemfRSTr0G9pEu/ofmIWwaJPfR09QyK+ZBFo/ffaApGbCbbNgQ4pjOf2NrglAJbqGpGSz5f3ACsn7vKXYvYAOM+1eodJb2mY2a+tk7qDjTN/gDAGgZ4O9oXE3o4ELf5qlqbos56WtU1xsJEzWkyiX8FYdBkvWX/00JL0aY+X89jTkDKP/IhGw+5BobkIO2VshXi9md3MUC4Hym3SB4TWSNAbqBgMY7tMBOfKEaVZTBf3gIrYP4dzOFFW5yXQ0q+ENCeiLn9mloCLOp+xfIpqZbe4vqGdZ3nmSO6jvQ/Iv8o7d5F1YGPNURcI2XPhouHa/32qr9mKpqLSnZk89Xq5HKmpzLDDR5X9YkSe1hesN3CtOochPEzZuEfW2wDP4ujcpm4u0eW3UFkOBPpekKZowJSjXgvw+qG9KvPe/K8iMcrwHNXPKB5SvcAyx8iIkOLw4aDhEvCcBf3ekjdLvefEa7XuvGCfWRiSZEXQZuR7HRfEsffjqInrfn5Yi9DpLbzsN6IyPV1Kj1qu537bK44+VoC56cgDGgIMf8ysL5/gOeM2pqWQ3uQ6iWjUMH4S659BCa9cBmryWhep8gtmUn6/B9wIWiqByPVV4jKPpzwSqAbY0NYYJ/AbtDlEWtlsSOeCZgoRO8kM3eNfW4u83rz+BKqY76cJ9SBfV6oc0x15vyd/6Pc1/pU0tK0z4KGpv8cqnF4mr6E8jLXkLDFakex5yGAJlJnihDR9r02Ygl8vAJN8hJjZwtTk/a+Q/Zeu4KW/ZXhlHb4DVDNww0WFdvtFmHdQQZ+ziTen7l66HckK+uFx2cx6dqilwkIKGeS0cvvCeRryHCOoz2A4qEd3H2j61oOHdb+iEtzxVVZ3F0IWG+3aHCdBPvvOhRwd9WqUK7VxxK0AVRxVUylHt4wyLElnmuB6k1h0NByCzI1n6funSXXLwqJcKvl/2bINOFWA/WoNPOuB+aKEK/1tMfgEpgAIfhPCf9DavNmf6CIT2YAI8futqWM/AXdE7wbAnhEqeyflfuQoxxsgWMTw1e06Y31Q/5ozVx8dTWFlG/eeb0/A+73mPJu97VLO6diexlqMgKZIQ/0YARRccVvHq5r1BhGeM5++aUhGjUdRqHvPXxl1lgeQ0kqTFbxBLRU6O6hPTPR9rNFj4BQmTJG3t4qkO7ARWRXXzqVAQFu6L6DI2hkdqC63vOi6XMYUXpGqp7y0TKE9z6HpFQchhYVUe36hEAiwr+umsN63TJyThqcR+elbFSJgry+MTl0K2NErYADjFdyJUMobMQkqbDLYb+vOyxOY4U/wqELCwRr833WmqedB6OEYMIcP/9hfj0HBPSjMNzJ8DpQzfV34nQjekbzwx87ugEaX/826qHAxkYFhhIXSPQKo=";
     ChirpConnect chirpConnect = new ChirpConnect(this, KEY, SECRET);
     String bitString;
+    private static String ACK = "00";
+    private static String NACK = "0";
     LinearLayout ll;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,8 +57,12 @@ public class ReceivingActivity extends AppCompatActivity {
 
             @Override
             public void onReceived(byte[] payload, byte channel) {
-                String temp = new String(payload);
-                final Integer seqNo = Integer.parseInt(temp.substring(temp.length() - 1));
+                if (payload == null) {
+                    return;
+                }
+                String temp = stripPadding(payload);
+
+                final int seqNo = Integer.parseInt(temp.substring(temp.length() - 1));
                 bitString = temp.substring(0, temp.length() - 1);
                 if (CRCToolkit.error_d(bitString.length(), bitString)) {
                     runOnUiThread(new Runnable() {
@@ -84,10 +90,10 @@ public class ReceivingActivity extends AppCompatActivity {
                             {
                                 Toast.makeText(getApplicationContext(), "Message 1 Received!", Toast.LENGTH_SHORT).show();
                             }
+                            chirpConnect.send((ACK + seqNo).getBytes());
 //                            messageNo = (messageNo + 1) % 2;
                         }
                     });
-                    chirpConnect.send(("1" + seqNo).getBytes());
 
                 }
 
@@ -104,7 +110,7 @@ public class ReceivingActivity extends AppCompatActivity {
                             Toast.makeText(getApplicationContext(), "Message received has errors!", Toast.LENGTH_SHORT).show();
                         }
                     });
-                    chirpConnect.send(("0" + seqNo).getBytes());
+                    chirpConnect.send((NACK + seqNo).getBytes());
                 }
 
             }
@@ -170,7 +176,7 @@ public class ReceivingActivity extends AppCompatActivity {
         chirpConnect.stop();
     }
 
-    public void setBackgroundGreen() {
+    private void setBackgroundGreen() {
         ll = findViewById(R.id.ll);
         int colorFrom = ((ColorDrawable)ll.getBackground()).getColor();
         int colorTo = Color.GREEN;
@@ -185,5 +191,24 @@ public class ReceivingActivity extends AppCompatActivity {
 
         });
         colorAnimation.start();
+    }
+
+    private static String stripPadding(final byte[] b) {
+        final char[] bits = new char[8 * b.length];
+        for(int i = 0; i < b.length; i++) {
+            final byte byteval = b[i];
+            int bytei = i << 3;
+            int mask = 0x1;
+            for(int j = 7; j >= 0; j--) {
+                final int bitval = byteval & mask;
+                if(bitval == 0) {
+                    bits[bytei + j] = '0';
+                } else {
+                    bits[bytei + j] = '1';
+                }
+                mask <<= 1;
+            }
+        }
+        return String.valueOf(bits).replaceFirst("^0*1", "");
     }
 }
